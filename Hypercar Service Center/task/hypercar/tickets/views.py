79123e4acd21f95baf8a1fe47c3910a3223fcdd4
Paweł Palacz <ppalacz@edu.cdv.pl>
2020-05-23 20:00:42 +0200

    Chapter 2 of 5 completed

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,7 +1,20 @@
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.http.response import HttpResponse
+from django.shortcuts import render
+
+services = {
+    'change_oil': 'Change oil',
+    'inflate_tires': 'Inflate tires',
+    'diagnostic': 'Get diagnostic test'
+}
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tickets/menu.html', context={'services': services})
